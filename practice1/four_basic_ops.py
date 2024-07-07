74ca904292d283a0df38009189ca5bda4b566002
JIM00N <100524386+JIM00N@users.noreply.github.com>
2024-07-07 14:54:00 +0900

    solve BJ-10869 (#2)

---
+++ practice1/four_basic_ops.py
@@ -0,0 +1,13 @@
+# Ploblem : [사칙연산] https://www.acmicpc.net/problem/10869
+# Solver : 문지석 (jimoon@gachon.ac.kr)
+# Solved Date : 2024.07.07 
+# BigO: 1
+
+input_list = input().split()
+a, b = int(input_list[0]), int(input_list[1])
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a // b)
+print(a % b)
