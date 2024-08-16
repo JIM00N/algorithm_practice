2b777b6412dcdcc6dd790306885f56b7e4571d4f
JIM00N <giants1940@gmail.com>
2024-08-16 15:16:06 +0900

    solve BJ-2447

---
+++ recursive/star_marking10.py
@@ -0,0 +1,63 @@
+class star_marking_10():
+    def __init__(self, n):
+        self.n = n
+        self.star_list = ["*", "***"]
+        self.a = f"{self.star_list[0]}" + self.blank(1) + f"{self.star_list[0]}"
+        self.star_marked = self.star_list[1] + "\n" + self.a + "\n" + self.star_list[1]
+        self.count = 1
+    def star_marking(self, n):
+        if n==3:
+            return self.star_marked
+        else:
+            mul_a = self.multi_a(3**self.count)
+            self.count += 1
+            self.star_marked = self.threeTimes_1(self.star_marked) + "\n" + mul_a + "\n" + self.threeTimes_1(self.star_marked)
+            return self.star_marking(n//3)
+
+    def blank(self, n):
+        bl = ' '*n
+        return bl
+
+    def threeTimes_1(self, stars):
+        strs = ""
+        strs_long = ""
+        for i in stars:
+            if i=="\n":
+                strs_long += (3*strs + i)
+                strs = ""
+            else:
+                strs += i
+        
+        strs_long += 3*strs
+        return strs_long
+        
+    def threeTimes_2(self, stars):
+        strs = ""
+        for i in stars:
+            if i=="\n":
+                continue
+            else:
+                strs += i*3
+        
+        return strs
+    
+    def multi_a(self, k):
+        self.a = self.star_marked
+        mul_a = ""
+        tmp = ""
+        for i in self.a:
+            if i=="\n":
+                tmp = tmp + self.blank(k) + tmp + i
+                mul_a += tmp
+                tmp = ""
+
+            else:
+                tmp += i
+        mul_a += tmp + self.blank(k) + tmp
+        return mul_a
+        # blank 건들여야함...
+
+n = int(input())
+stars = star_marking_10(n)
+result = stars.star_marking(stars.n)
+print(result)
