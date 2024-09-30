bf88df2470a622e465000513f8c224137d36c517
JIM00N <giants1940@gmail.com>
2024-09-30 09:32:10 +0900

    add a comment and BigO, and switch up and down in method move

--- graph/war_combat.py
+++ graph/war_combat.py
@@ -1,7 +1,7 @@
 # Problem : [전쟁-전투] https://www.acmicpc.net/problem/1303
 # Solver : 문지석
 # Solved Date : 2024.09.28
-# BigO : 
+# BigO : n ** 2
 import copy
 
 
@@ -29,26 +29,30 @@ class war:
                     
     def move(self, pos, team):
         col, row = pos[0], pos[1]
-        up = [col - 1, row]
-        down = [col + 1, row]
+        up = [col + 1, row]
+        down = [col - 1, row]
         right = [col, row + 1]
         left = [col, row - 1]
         
-        if up[0] >= 0 and self.visit[up[0]][up[1]] == False:
+        # 위 아래 오른쪽 왼쪽으로 움직이고 그 자리에서 재귀
+        if up[0] < self.len_col and self.visit[up[0]][up[1]] == False:
             if self.matrix[up[0]][up[1]] == team:
                 self.my_team += 1
                 self.visit[up[0]][up[1]] = True
                 self.move([up[0], up[1]], team)
-        if down[0] < self.len_col and self.visit[down[0]][down[1]] == False:
+
+        if down[0] >= 0 and self.visit[down[0]][down[1]] == False:
             if self.matrix[down[0]][down[1]] == team:
                 self.my_team += 1
                 self.visit[down[0]][down[1]] = True
                 self.move([down[0], down[1]], team)
+
         if right[1] < self.len_row and self.visit[right[0]][right[1]] == False:
             if self.matrix[right[0]][right[1]] == team:
                 self.my_team += 1
                 self.visit[right[0]][right[1]] = True
                 self.move([right[0], right[1]], team)
+
         if left[1] >= 0 and self.visit[left[0]][left[1]] == False:
             if self.matrix[left[0]][left[1]] == team:
                 self.my_team += 1
