703aac5753ef8b5e4210ab92c4be569a3bdccc0f
JIM00N <giants1940@gmail.com>
2024-10-08 20:49:23 +0900

    solve BJ-11727

--- DP/2n_tiling2.py
+++ DP/2n_tiling2.py
@@ -7,11 +7,8 @@
 def tiling2(n):
     seq = [1, 3]
     for i in range(3, n + 1):
-        if i % 2 == 0:
-            next = 4 * seq[i - 3] - 1
-        else:
-            next = 2 * seq[i - 2] - 1
-
+        # a_k = a_k-1 + 3 * a_k-2 - a_k-2 = a_k-1 + 2 * a_k-2
+        next = seq[-1] + 2 * seq[-2]
         next %= 10007
         seq.append(next)
 
