bea0f65b43ae520da001b6634511852718b3391a
JIM00N <giants1940@gmail.com>
2024-11-12 13:19:22 +0900

    add comments in BJ-1715

--- heap/card_sorting.py
+++ heap/card_sorting.py
@@ -9,8 +9,8 @@ import sys
 def min_compare(repitition, decks):
     sum_list = []
 
-    if len(decks) == 1:
-        heapq.heappop(decks)
+    if len(decks) == 1:# 카드 뭉치가 하나라면 비교할 필요가 없음 따라서 0 추가
+        heapq.heappop(decks) # while을 거치지 않도록 pop
         sum_list.append(0)
 
     while len(decks) != 0:
@@ -18,8 +18,8 @@ def min_compare(repitition, decks):
             val_add = heapq.heappop(decks) + heapq.heappop(decks)
             sum_list.append(val_add)
         else:
-            if sum_list[-1] > decks[0]:
-                heapq.heappush(decks, sum_list[-1])
+            if sum_list[-1] > decks[0]: # 두개를 더 했을 때 다음에 더할 카드보다 크다면
+                heapq.heappush(decks, sum_list[-1]) # 다시 더할 카드 목록에 추가
                 val_add = heapq.heappop(decks) + heapq.heappop(decks)
                 sum_list.append(val_add)
             else:
