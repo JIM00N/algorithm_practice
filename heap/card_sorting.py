5ff8697a72528705a06a2ddc47ae2206332126d4
JIM00N <giants1940@gmail.com>
2024-11-12 13:12:30 +0900

    add bigO and black BJ-1715

--- heap/card_sorting.py
+++ heap/card_sorting.py
@@ -1,9 +1,10 @@
 # Problem : [카드 정렬하기] https://www.acmicpc.net/problem/1715
 # Solver : 문지석
 # Solved Date : 2024.11.12
-# BigO : 
+# BigO : N * logN
 import heapq
 import sys
+
 
 def min_compare(repitition, decks):
     sum_list = []
@@ -12,7 +13,7 @@ def min_compare(repitition, decks):
         heapq.heappop(decks)
         sum_list.append(0)
 
-    while(len(decks) != 0):
+    while len(decks) != 0:
         if len(sum_list) == 0:
             val_add = heapq.heappop(decks) + heapq.heappop(decks)
             sum_list.append(val_add)
@@ -25,9 +26,9 @@ def min_compare(repitition, decks):
                 sum_list.append(sum_list[-1] + heapq.heappop(decks))
 
     return sum(sum_list)
-        
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     input = sys.stdin.read
     data = list(map(int, input().split()))
     data_heap = data[1:]
