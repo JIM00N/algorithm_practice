425e570a0e20d1e6fbf6c57f2d5745c42c34c543
JIM00N <giants1940@gmail.com>
2025-01-06 16:03:35 +0900

    solve BJ-2579 계단오르기

--- DP/stairs.py
+++ DP/stairs.py
@@ -1,35 +1,30 @@
 # Problem : [계단 오르기] https://www.acmicpc.net/problem/2579
 # Solver : 문지석
-# Solved Date : 2024.10.06
+# Solved Date : 2025.01.06
 # BigO : n
 
-# def num_step(n):
-#     step_count = [1, 2, 2]
-#     if n <= 3:
-#         return step_count[n-1]
-#     for i in range(4, n+1):
-#         if i % 3 == 1:
-#             step_count.append(step_count[i - 2] + 1)
-#         elif i % 3 == 2:
-#             step_count.append(step_count[i - 3] + 2)
-#         else:
-#             step_count.append(step_count[-1])
-    
-#     return step_count[-1]
-            
-def max_score(score_list):
-    length = len(score_list)
-    # steps = num_step(length)
-    
 
-if __name__=="__main__":
+def max_score(stairs):
+    if len(stairs) == 2:
+        return stairs[0] + stairs[1]
+    elif len(stairs) == 1:
+        return stairs[0]
+
+    scores = []
+    scores.append([stairs[0], stairs[0]])
+    scores.append([stairs[0] + stairs[1], stairs[1]])
+
+    idx = 2
+    while len(scores) != len(stairs):
+        o_prev = scores[idx - 1][-1] + stairs[idx]
+        x_prev = max(scores[idx - 2]) + stairs[idx]
+        scores.append([o_prev, x_prev])
+        idx += 1
+
+    return max(scores[-1])
+
+
+if __name__ == "__main__":
     num_stairs = int(input())
-    scores = [int(input()) for _ in range(num_stairs)]
-
-
-'''
-n = 
-1) n - 2 
-2) n - 1 +>> n - 3
-dp를 사용하려면 리스트 두개를 만들고 서로 더해가자 n-2와 n-3을 두개 같이 계산하면 안돼
-'''
+    stairs = [int(input()) for _ in range(num_stairs)]
+    print(max_score(stairs))
