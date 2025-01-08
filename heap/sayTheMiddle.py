5a5fb77ff13bcdbd3eacf0037e76b603bcc72a20
JIM00N <giants1940@gmail.com>
2025-01-08 20:47:49 +0900

    too high time complexity

--- heap/sayTheMiddle.py
+++ heap/sayTheMiddle.py
@@ -4,28 +4,27 @@
 # BigO : 
 import heapq
 import sys
-import copy
-
 
 def searchTheMiddle(heap_len):
     if heap_len & 0x0001:
-        return heap_len >> 1
+        return (heap_len - 1) >> 1
     else:
         return (heap_len >> 1) - 1
 
-def sayTheMiddle(heap, data): 
+def sayTheMiddle(data):
     repitition = data[0] + 1
-    for idx in range(1, repitition):
-        heapq.heappush(heap, data[idx])
-        replicated_heap = copy.deepcopy(heap)
-        for _ in range(searchTheMiddle(len(heap))):
-            heapq.heappop(replicated_heap)
+    data = data[1:]
+    # heapq.heapify(data)
+    for i in range(1, repitition):
+        tmp_data = data[:i]
+        heapq.heapify(tmp_data)
+        for _ in range(searchTheMiddle(i)):
+            heapq.heappop(tmp_data)
 
-        print(replicated_heap[0])
+        print(tmp_data[0])
 
 
 if __name__=="__main__":
     input = sys.stdin.read
     data = list(map(int, input().split()))
-    my_heap = []
-    sayTheMiddle(my_heap, data)
+    sayTheMiddle(data)
