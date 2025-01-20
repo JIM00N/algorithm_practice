3e4984d3a8208d6d8066bda6fc3ac0936892b6de
JIM00N <giants1940@gmail.com>
2025-01-20 15:15:39 +0900

    simplify, Kruskal MST with union find

--- MST/MST_kruskal_union_find.py
+++ MST/MST_kruskal_union_find.py
@@ -5,41 +5,26 @@ class MST:
         self.num = num_nodes
         edges.sort()
         self.edges = edges
-        self.parent = {chr(97+i): [chr(97+i), 0] for i in range(self.num)}
+        self.parent = {chr(97 + i): [chr(97 + i), 0] for i in range(self.num)}
         self.sum = 0
-        
 
     def union(self):
         for edge in self.edges:
             weight, x, y = edge[0], edge[1], edge[2]
             mode = self.parent[x][-1] + self.parent[y][-1]
-            if mode == 0:
+            
+            if self.parent[x][-1] != 1 or self.parent[y][-1] != 1:
                 self.parent[x][-1] = 1
                 self.parent[y][-1] = 1
-                self.parent[y][0] = x
                 self.sum += weight
-
-            elif self.parent[x][-1] == 1 and mode == 1:
-                self.parent[y][-1] = 1
-                self.parent[y][0] = x
-                self.sum += weight
-
-            elif self.parent[y][-1] == 1 and mode == 1:
-                self.parent[x][-1] = 1
-                self.parent[x][0] = y
-                self.sum += weight
-            # if self.parent[x][-1] != 1 or self.parent[y][-1] != 1:
-            #     self.parent[x][-1] = 1
-            #     self.parent[y][-1] = 1
-            #     self.sum += weight
-            #     if self.parent[x][-1] == 1 or mode == 0:
-            #         self.parent[y][0] = x
-            #     else:
-            #         self.parent[x][0] = y
+                if self.parent[x][-1] == 1 or mode == 0:
+                    self.parent[y][0] = x
+                else:
+                    self.parent[x][0] = y
 
             else:
                 x_root, y_root = self.find(x), self.find(y)
-                
+
                 if x_root < y_root:
                     self.change_parent(y, y_root)
                     self.parent[y][0] = x
@@ -48,14 +33,13 @@ class MST:
                     self.change_parent(x, x_root)
                     self.parent[x][0] = y
                     self.sum += weight
-                
 
     def find(self, node):
         if self.parent[node][0] == node:
             return node
         else:
             return self.find(self.parent[node][0])
-        
+
     def change_parent(self, node, root_node, prev_node=None):
         if node == root_node:
             self.parent[node][0] = prev_node
@@ -76,7 +60,6 @@ if __name__ == "__main__":
         edge_list.append([int(weight), org, dst])
 
     mst = MST(num_nodes, edge_list)
-    print(edge_list)
     mst.union()
     print(mst.sum)
     print(mst.parent)
