731bb362ef7dc1499e667a7cd5e81a0404d3c21e
JIM00N <giants1940@gmail.com>
2025-02-18 11:51:32 +0900

    solve BJ-1655

--- heap/sayTheMiddle.py
+++ heap/sayTheMiddle.py
@@ -11,17 +11,18 @@ def sayTheMiddle(data):
     data = data[1:]
 
     mid = data[0]
-    smaller_heap = []  # max heap
-    bigger_heap = []  # min heap
+    smaller_heap = [] # max heap
+    bigger_heap = [] # min heap
+
     for i in range(repitition):
         tmp_data = data[i]
         if tmp_data >= mid:
             heapq.heappush(bigger_heap, tmp_data)
         else:
             heapq.heappush(smaller_heap, (-1) * tmp_data)
-
+        
         gap = len(smaller_heap) - len(bigger_heap)
-
+        
         if gap == 0 or gap == 1:
             mid = smaller_heap[0] * (-1)
             print(mid)
@@ -35,8 +36,7 @@ def sayTheMiddle(data):
             mid = smaller_heap[0] * (-1)
             print(mid)
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     input = sys.stdin.read
     data = list(map(int, input().split()))
     sayTheMiddle(data)
