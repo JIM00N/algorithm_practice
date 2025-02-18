77d43c41bf60de5e35c18b1b2737f2e97b6083ee
JIM00N <giants1940@gmail.com>
2025-02-18 11:49:49 +0900

    solve BJ-1655

--- heap/sayTheMiddle.py
+++ heap/sayTheMiddle.py
@@ -1,28 +1,42 @@
 # Problem : [가운데를 말해요] https://www.acmicpc.net/problem/1655
 # Solver : 문지석
-# Solved Date : 2025.02.17
-# BigO : 
+# Solved Date : 2025.02.18
+# BigO : N * log(N)
 import heapq
 import sys
 
-def searchTheMiddle(heap_len):
-    if heap_len & 0x0001:
-        return (heap_len - 1) >> 1
-    else:
-        return (heap_len >> 1) - 1
 
 def sayTheMiddle(data):
-    repitition = data[0] + 1
+    repitition = data[0]
     data = data[1:]
 
-    for i in range(1, repitition):
-        tmp_data = data[:i]
-        n = searchTheMiddle(i)
-        result = heapq.nsmallest(n + 1, tmp_data)
-        print(result[-1])
+    mid = data[0]
+    smaller_heap = []  # max heap
+    bigger_heap = []  # min heap
+    for i in range(repitition):
+        tmp_data = data[i]
+        if tmp_data >= mid:
+            heapq.heappush(bigger_heap, tmp_data)
+        else:
+            heapq.heappush(smaller_heap, (-1) * tmp_data)
+
+        gap = len(smaller_heap) - len(bigger_heap)
+
+        if gap == 0 or gap == 1:
+            mid = smaller_heap[0] * (-1)
+            print(mid)
+        elif gap == -1:
+            mid = heapq.heappop(bigger_heap)
+            heapq.heappush(smaller_heap, (-1) * mid)
+            print(mid)
+        elif gap == 2:
+            transfer = heapq.heappop(smaller_heap)
+            heapq.heappush(bigger_heap, (-1) * transfer)
+            mid = smaller_heap[0] * (-1)
+            print(mid)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     input = sys.stdin.read
     data = list(map(int, input().split()))
     sayTheMiddle(data)
