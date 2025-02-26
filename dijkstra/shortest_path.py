091ae3d3c855c57894a8ff09c082141390ae7277
JIM00N <giants1940@gmail.com>
2025-02-26 16:44:45 +0900

    solve BJ-2753

--- dijkstra/shortest_path.py
+++ dijkstra/shortest_path.py
@@ -1,7 +1,7 @@
 # Problem : [최단 경로] https://www.acmicpc.net/problem/1753
 # Solver : 문지석
-# Solved Date : 2024.11. 17
-# BigO :
+# Solved Date : 2025.02.26
+# BigO : VlogV+ElogV
 
 
 import heapq
@@ -11,43 +11,47 @@ class Dijkstra:
     def __init__(self, data, start, num):
         self.data = data
         self.start = start
-        self.result_dict = {i+1 : float("INF") for i in range(num)}
+        self.result_dict = {i+1 : float("INF") for i in range(num)} # self.start -> another node
         self.buffer = []
-        self.visit = [[False] for _ in range(num)]
+        self.visit = [False for _ in range(num)]
+
+    def initialize_dict(self):
+        self.result_dict[self.start ] = 0
+        self.visit[self.start - 1] = True
+        for dst, weight in self.data[self.start].items():
+            self.result_dict[dst] = min(self.result_dict[dst], weight)
+            heapq.heappush(self.buffer, (weight, dst))
 
     def path_finder(self):
-        for idx, weight in enumerate(self.data[self.start - 1]):
-            self.result_dict[idx + 1] = weight
-            heapq.heappush(self.buffer, (weight, idx + 1)) #idx가 먼저일까 weight이 먼저일까
-        
-        while self.buffer or (False in self.visit):
-            org_weight, tmp_node = heapq.heappop(self.buffer)
-            self.visit[tmp_node - 1] = True
-            for idx, tmp_weight in enumerate(self.data[tmp_node - 1]):
-                if tmp_weight != 0 and tmp_weight != float("INF"):
-                    add_weight = org_weight + tmp_weight
-                    if self.result_dict[idx + 1] > add_weight:
-                        self.result_dict[idx + 1] = add_weight
-                        heapq.heappush(self.buffer, (add_weight, idx + 1))
+        while self.buffer:
+            org_weight, cur_node = heapq.heappop(self.buffer)
+            if self.visit[cur_node - 1] == True:
+                continue
+            self.visit[cur_node - 1] = True
+            self.result_dict[cur_node] = min(org_weight, self.result_dict[cur_node])
 
+            for dst, tmp_weight in self.data[cur_node].items():
+                total_weight = org_weight + tmp_weight
+                if total_weight < self.result_dict[dst]:
+                    heapq.heappush(self.buffer, (total_weight, dst))
 
 if __name__ == "__main__":
     input = sys.stdin.read
-    data = list(input().split("\n"))
+    data = list(input().strip().split("\n"))
     num_nodes, s_node = int(data[0].split()[0]), int(data[1])
     
-    paths = [[float("INF")] * num_nodes for _ in range(num_nodes)]
-    for idx in range(num_nodes):
-        paths[idx][idx] = 0
+    data_dict = {i+1: {} for i in range(num_nodes)}
+    for u_v_w in data[2:]:
+        u_v_w = list(map(int, u_v_w.split()))
+        u, v, w= u_v_w[0], u_v_w[1], u_v_w[2]
+        try:
+            data_dict[u][v] = min(data_dict[u][v], w)
+        except:
+            data_dict[u][v] = w
 
-    for path in data[2:]:
-        if not path.strip():  # 빈 줄 무시
-            continue
-        u, v, w = tuple(map(int, path.split()))
-        paths[u - 1][v - 1] = min(paths[u - 1][v - 1], w)
-    
-    
-    shortest_path = Dijkstra(paths, s_node, num_nodes)
+
+    shortest_path = Dijkstra(data_dict, s_node, num_nodes)
+    shortest_path.initialize_dict()
     shortest_path.path_finder()
 
     for weight in list(shortest_path.result_dict.values()):
