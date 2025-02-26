75fe38fced273f68cb1317f0901a7edb9b8958e9
JIM00N <giants1940@gmail.com>
2025-02-26 15:14:02 +0900

    memory exceeded

--- dijkstra/shortest_path.py
+++ dijkstra/shortest_path.py
@@ -7,32 +7,50 @@
 import heapq
 import sys
 
+class Dijkstra:
+    def __init__(self, data, start, num):
+        self.data = data
+        self.start = start
+        self.result_dict = {i+1 : float("INF") for i in range(num)}
+        self.buffer = []
+        self.visit = [[False] for _ in range(num)]
 
-def dijkstra(graph, start):
-    visit = set()
-    result = dict()
-    p_queue = []
-    p_queue.append(start)
-    visit.add()
-
-    while len(p_queue) != 0:  # BFS
+    def path_finder(self):
+        for idx, weight in enumerate(self.data[self.start - 1]):
+            self.result_dict[idx + 1] = weight
+            heapq.heappush(self.buffer, (weight, idx + 1)) #idx가 먼저일까 weight이 먼저일까
         
-
-    return result
+        while self.buffer or (False in self.visit):
+            org_weight, tmp_node = heapq.heappop(self.buffer)
+            self.visit[tmp_node - 1] = True
+            for idx, tmp_weight in enumerate(self.data[tmp_node - 1]):
+                if tmp_weight != 0 and tmp_weight != float("INF"):
+                    add_weight = org_weight + tmp_weight
+                    if self.result_dict[idx + 1] > add_weight:
+                        self.result_dict[idx + 1] = add_weight
+                        heapq.heappush(self.buffer, (add_weight, idx + 1))
 
 
 if __name__ == "__main__":
     input = sys.stdin.read
     data = list(input().split("\n"))
-    num_nodes, s_node = int(data[0][0]), int(data[1])
+    num_nodes, s_node = int(data[0].split()[0]), int(data[1])
     
-    paths = [[float("INF")] * 5 for x in range(5)]
-    paths[0][0] = 1
+    paths = [[float("INF")] * num_nodes for _ in range(num_nodes)]
+    for idx in range(num_nodes):
+        paths[idx][idx] = 0
 
     for path in data[2:]:
         if not path.strip():  # 빈 줄 무시
             continue
         u, v, w = tuple(map(int, path.split()))
-        paths[u][v] = w
+        paths[u - 1][v - 1] = min(paths[u - 1][v - 1], w)
+    
+    
+    shortest_path = Dijkstra(paths, s_node, num_nodes)
+    shortest_path.path_finder()
 
-    print(dijkstra(paths, s_node))
+    for weight in list(shortest_path.result_dict.values()):
+        if weight == float("INF"):
+            weight = "INF"
+        print(weight)
