a0ef8ed5b0b19b86222e4616678f55e31e45172a
JIM00N <giants1940@gmail.com>
2025-03-18 19:20:17 +0900

    delete origin file and make new version with small improvements

--- BST/BST_pre2post.py
+++ BST/BST_pre2post.py
@@ -9,38 +9,34 @@ class BST:
     def __init__(self):
         self.po_list = []
 
-    def post_order(self, tmp_tree, start=0):
+    def pre2post(self, tmp_tree):
+        root = tmp_tree[0]  # pre_order로 주어지니 root는 가장 앞
+
         if len(tmp_tree) == 1:
-            self.po_list.append(f"{tmp_tree[start]}")
+            self.po_list.append(f"{tmp_tree[0]}")
             return
+        
+        # Left subtree와 right subtree가 나뉘는 idx 찾기
+        cut_idx = 1
 
-        root = tmp_tree[start]  # pre_order로 주어지니 root는 가장 앞
-
-        # 인덱스 초기화
-        left_child_idx = 1
-        right_child_idx = len(tmp_tree)
-
-        # LC와 RC를 찾고, 나눠주자
-        for idx, node in enumerate(tmp_tree):
-            if node > root:
-                right_child_idx = idx
+        for val in tmp_tree[1:]:
+            if val > root: # right subtree가 나오는 순간 break
                 break
-            if node < root and left_child_idx == 0:
-                left_child_idx = idx
+            cut_idx += 1
 
-        tmp_left = tmp_tree[left_child_idx:right_child_idx]
-        tmp_right = tmp_tree[right_child_idx:]
+        tmp_left = tmp_tree[1:cut_idx]
+        tmp_right = tmp_tree[cut_idx:]
 
         if len(tmp_left) != 0 and len(tmp_right) != 0:
             # 양쪽 자식 트리가 모두 있을 때
-            self.post_order(tmp_left)  # 왼쪽 먼저, post_order
-            self.post_order(tmp_right)
+            self.pre2post(tmp_left)  # 왼쪽 먼저, pre -> post
+            self.pre2post(tmp_right)
         elif len(tmp_left) == 0 and len(tmp_right) != 0:
             # 오른쪽만 있을 때
-            self.post_order(tmp_right)
+            self.pre2post(tmp_right)
         elif len(tmp_left) != 0 and len(tmp_right) == 0:
             # 왼쪽만 있을 때
-            self.post_order(tmp_left)
+            self.pre2post(tmp_left)
 
         self.po_list.append(f"{root}")  # 자식들 추가한 뒤에 root 추가
 
@@ -55,5 +51,5 @@ if __name__ == "__main__":
             break
 
     pre_to_post = BST()
-    pre_to_post.post_order(pre_order)
+    pre_to_post.pre2post(pre_order)
     print("\n".join(pre_to_post.po_list))
