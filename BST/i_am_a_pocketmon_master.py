ab27222a5052c79bf72bcadb2e09f913881bd22b
JIM00N <100524386+JIM00N@users.noreply.github.com>
2025-08-10 14:48:25 +0900

    solve BJ-1620, pocket monster LeeDasom (#45)
    * solve BJ-1620, pocket monster LeeDasom
    
    * add BigO and make codes clear by black
    
    * BJ-1620, change variables
    
    * add comments

---
+++ BST/i_am_a_pocketmon_master.py
@@ -0,0 +1,76 @@
+# Problem : [나는야 포켓몬 마스터 이다솜] https://www.acmicpc.net/problem/1620
+# Solver : 문지석
+# Solved Date : 2025.02.05
+# BigO : N * log(N)
+from dataclasses import dataclass
+
+
+@dataclass
+class Pocket_Mon:
+    name: str
+    num: int
+    left_child: object = None
+    right_child: object = None
+
+
+class Pocketmon_Ball:
+    def __init__(self):
+        self.name_list = []
+        self.root: Pocket_Mon = None
+
+    def insert(self, mon: Pocket_Mon):
+        self.name_list.append(mon)
+
+        if len(self.name_list) == 1:
+            self.root = mon
+            return
+
+        cur_mon = self.root # 순회하기 위한 cur_mon 변수 생성
+        while cur_mon != None:
+            if cur_mon.name > mon.name:
+                # cur_mon 이름이 mon 이름보다 알파벳 순서 상 뒤라면
+                if cur_mon.left_child != None:
+                    cur_mon = cur_mon.left_child
+                else:
+                    cur_mon.left_child = mon
+                    return
+            elif cur_mon.name < mon.name:
+                # cur_mon 이름이 mon 이름보다 알파벳 순서 상 앞이라면
+                if cur_mon.right_child != None:
+                    cur_mon = cur_mon.right_child
+                else:
+                    cur_mon.right_child = mon
+                    return
+
+    def search_by_num(self, n: int): # 번호로 찾으면 name 리턴
+        return self.name_list[n - 1].name
+
+    def search_by_name(self, name: str): # 이름으로 찾으면 번호 리턴
+        cur_mon = self.root
+        while cur_mon.name != name:
+            if cur_mon.name > name:
+                cur_mon = cur_mon.left_child
+            elif cur_mon.name < name:
+                cur_mon = cur_mon.right_child
+
+        return cur_mon.num
+
+
+if __name__ == "__main__":
+    num_pocketmons, num_questions = tuple(map(int, input().split()))
+    lee_dasom = Pocketmon_Ball()
+
+    for i in range(num_pocketmons):
+        lee_dasom.insert(Pocket_Mon(input(), i + 1))
+        # (포켓몬 이름, 번호) 입력
+
+    result = []
+    for _ in range(num_questions):
+        question = input() # 찾는 포켓몬의 이름 혹은 번호
+        try:
+            question = int(question) # 번호이면 search_by_num
+            result.append(lee_dasom.search_by_num(question))
+        except: # 번호가 아니라 이름이라면 search_by_name
+            result.append(f"{lee_dasom.search_by_name(question)}")
+
+    print("\n".join(result))
